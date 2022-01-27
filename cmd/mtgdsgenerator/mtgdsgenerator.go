93e9db047a672ed56bd5baad49c1eefcfb49678a
Aleksey Sviridkin <asviridkin@ozon.ru>
2022-01-27 04:03:55 +0000

    style fix
    Signed-off-by: Aleksey Sviridkin <asviridkin@ozon.ru>

--- cmd/mtgdsgenerator/mtgdsgenerator.go
+++ cmd/mtgdsgenerator/mtgdsgenerator.go
@@ -56,6 +56,7 @@ func main() {
 
 func generateReport(result map[string][]string) {
 	log.Println("Generating report...")
+
 	jsondata, err := json.Marshal(result)
 	if err != nil {
 		log.Println(err)
