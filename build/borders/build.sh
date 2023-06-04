379e37718a518ba3fd64e191b1ad2cd7a80978fd
Aleksey Sviridkin <a.sviridkin@sdventures.com>
2023-06-04 17:47:10 +0300

    do not use upx for borders
    Signed-off-by: Aleksey Sviridkin <a.sviridkin@sdventures.com>

--- build/borders/build.sh
+++ build/borders/build.sh
@@ -2,12 +2,9 @@
 
 # Build for Windows (amd64)
 env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o borders-windows-amd64.exe main.go
-upx --lzma --best borders-windows-amd64.exe
 
 # Build for Mac (amd64)
 env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o borders-mac-amd64 main.go
-upx --lzma --best borders-mac-amd64
 
 # Build for Mac (arm64)
 env GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o borders-mac-arm64 main.go
-upx --lzma --best borders-mac-arm64
