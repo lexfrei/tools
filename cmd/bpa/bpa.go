4cd6d6c8a38ce5aa8ad0e1bf87033616ed1e89bb
Aleksei Sviridkin <aleksei.sviridkin@ringcentral.com>
2024-05-31 17:56:42 +0400

    chore: Refactor bpa.go to improve readability and error handling
    Signed-off-by: Aleksei Sviridkin <aleksei.sviridkin@ringcentral.com>

--- cmd/bpa/bpa.go
+++ cmd/bpa/bpa.go
@@ -6,20 +6,40 @@ import (
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/cockroachdb/errors"
 )
 
 func main() {
-	line := os.Args[1]
+	key1 := os.Args[1]
+	hey2 := os.Args[2]
 
-	for len(line)%4 != 0 {
-		line += "="
+	if len(key1) == 0 || len(hey2) == 0 {
+		log.Fatal("key1 and key2 must be provided")
 	}
 
-	data, err := base64.StdEncoding.DecodeString(line)
+	encodedKey1, err := b64ToHex(key1)
 	if err != nil {
-		log.Fatal("Cannot decode base64: ", err)
+		log.Fatal("cannot decode key1: ", err)
+	}
 
-		return
+	encodedKey2, err := b64ToHex(hey2)
+	if err != nil {
+		log.Fatal("cannot decode key2: ", err)
+	}
+
+	//nolint:forbidigo // This is a command line tool
+	fmt.Printf("%s:%s\n", encodedKey2, encodedKey1)
+}
+
+func b64ToHex(b64 string) (string, error) {
+	for len(b64)%4 != 0 {
+		b64 += "="
+	}
+
+	data, err := base64.StdEncoding.DecodeString(b64)
+	if err != nil {
+		return "", errors.Wrap(err, "cannot decode base64")
 	}
 
 	var result string
@@ -28,6 +48,5 @@ func main() {
 		result += strconv.FormatUint(uint64(r), 16)
 	}
 
-	//nolint:forbidigo // This is a command line tool
-	fmt.Println(result)
+	return result, nil
 }
